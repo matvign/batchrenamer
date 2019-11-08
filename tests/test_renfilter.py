3ad45717b95bb9897cf7932549e9980498aad2a6
Andrew Au <andrew.ch.au@outlook.com>
2019-11-08 22:06:28 +1100

    update test_renfilter tests

--- tests/test_renfilter.py
+++ tests/test_renfilter.py
@@ -101,269 +101,109 @@ def test_filter_postpend(post_arg, post_src, post_dest):
     assert dest == post_dest
 
 
+@pytest.mark.parametrize("sp_arg, sp_src, sp_dest", [
+    ("", ["file", "file a", "file b"], ["file", "filea", "fileb"]),
+    (".", ["file", "file a", "file b"], ["file", "file.a", "file.b"])
+])
+def test_filter_spaces(sp_arg, sp_src, sp_dest):
+    """Tests for spaces argument. Replace spaces with the specified character """
+    args = parser.parse_args(["-sp", sp_arg])
+    filters = renamer.initfilters(args)
+    dest = renamer.get_renames(sp_src, filters, args.extension, args.raw)
+    print(dest)
+    print(sp_dest)
+    assert dest == sp_dest
+
+
+def test_filter_spaces_noarg():
+    """Special test case for spaces without arguments (defaults to '_') """
+    args = parser.parse_args(["-sp"])
+    filters = renamer.initfilters(args)
+    dest = renamer.get_renames(["file a", "file b"], filters, args.extension, args.raw)
+    assert dest == ["file_a", "file_b"]
+
+
+@pytest.mark.parametrize("c_arg, c_src, c_dest", [
+    ("upper", ["file", "FILE1"], ["FILE", "FILE1"]),
+    ("upper", ["file1"], ["FILE1"]),
+    ("lower", ["FILE", "FILE2"], ["file", "file2"]),
+    ("lower", ["FiLe", "FiLe2"], ["file", "file2"]),
+    ("swap", ["FILE", "file"], ["file", "FILE"]),
+    ("swap", ["fiLE1", "FIle2"], ["FIle1", "fiLE2"]),
+    ("cap", ["FileName", "file name"], ["Filename", "File Name"]),
+    ("cap", ["file1 1name", "file1_1name"], ["File1 1Name", "File1_1Name"]),
+])
+def test_filter_case(c_arg, c_src, c_dest):
+    """Tests for case argument. Switch case depending on argument """
+    args = parser.parse_args(["-c", c_arg])
+    filters = renamer.initfilters(args)
+    dest = renamer.get_renames(c_src, filters, args.extension, args.raw)
+    assert dest == c_dest
+
+
+@pytest.mark.parametrize("tr_arg, tr_src, tr_dest", [
+    (["a", "b"], ["filea"], ["fileb"]),
+    (["fle", "tbi"], ["filea"], ["tibia"]),
+    (["1", "2"], ["file1"], ["file2"])
+])
+def test_filter_translate(tr_arg, tr_src, tr_dest):
+    """Tests for translate argument. Translate characters from one to another """
+    args = parser.parse_args(["-tr", *tr_arg])
+    filters = renamer.initfilters(args)
+    dest = renamer.get_renames(tr_src, filters, args.extension, args.raw)
+    assert dest == tr_dest
+
+
+@pytest.mark.parametrize("sl_arg, sl_src, sl_dest", [
+    (":4", ["filea"], ["file"]),
+    ("1:5", ["filea"], ["ilea"]),
+    ("2:", ["filea"], ["lea"])
+])
+def test_filter_slice(sl_arg, sl_src, sl_dest):
+    """Tests for slice argument. Slice portion of filename as the new name """
+    args = parser.parse_args(["-sl", sl_arg])
+    filters = renamer.initfilters(args)
+    dest = renamer.get_renames(sl_src, filters, args.extension, args.raw)
+    assert dest == sl_dest
+
+
+@pytest.mark.parametrize("sh_arg, sh_src, sh_dest", [
+    (":1", ["filea"], ["file"]),
+    ("1:", ["filea"], ["ilea"]),
+    ("1:1", ["filea"], ["ile"])
+])
+def test_filter_shave(sh_arg, sh_src, sh_dest):
+    """Tests for shave argument. Shave away portion of filename """
+    args = parser.parse_args(["-sh", sh_arg])
+    filters = renamer.initfilters(args)
+    dest = renamer.get_renames(sh_src, filters, args.extension, args.raw)
+    assert dest == sh_dest
+
+
+@pytest.mark.parametrize("bracr_arg, bracr_src, bracr_dest", [
+    (["round "], ["filea"], ["filea"]),
+    (["round"], ["(filea)file(file)", "(file)(file)(file)file"], ["file", "file"]),
+    (["round", "0"], ["(file)(file)(file)file"], ["file"]),
+    (["round", "1"], ["(file)file(file)"], ["file(file)"]),
+    (["round", "2"], ["(file)file(file)"], ["(file)file"]),
+    (["round", "3"], ["(file)file(file)"], ["(file)file(file)"]),
+    (["square", "1"], ["[file]file[file]"], ["file[file]"]),
+    (["square", "2"], ["[file]file[file]"], ["[file]file"]),
+    (["square", "3"], ["[file]file[file]"], ["[file]file[file]"]),
+    (["curly", "1"], ["{file}file{file}"], ["file{file}"]),
+    (["curly", "2"], ["{file}file{file}"], ["{file}file"]),
+    (["curly", "3"], ["{file}file{file}"], ["{file}file{file}"]),
+])
+def test_filter_bracr(bracr_arg, bracr_src, bracr_dest):
+    """Tests for bracket remove argument. Remove specified bracket type """
+    args = parser.parse_args(["-bracr", *bracr_arg])
+    filters = renamer.initfilters(args)
+    dest = renamer.get_renames(bracr_src, filters, args.extension, args.raw)
+    assert dest == bracr_dest
+
+
 """
-def pairwise(lst1, lst2, tmpdir=None):
-    if len(lst1) != len(lst2):
-        raise Exception('list lengths must be equal!')
-    it1, it2 = iter(lst1), iter(lst2)
-    try:
-        while True:
-            if not tmpdir:
-                yield next(it1), next(it2)
-            else:
-                yield tmpdir.join(next(it1)), tmpdir.join(next(it2))
-    except StopIteration:
-        return
-
-@pytest.mark.parametrize("pre_args, pre_before, pre_after", [
-    (['-pre', 'pre_', '-v'], ['file1', 'file2'], ['pre_file1', 'pre_file2']),
-    (['-pre', 'pre_', '-v'], ['FILE1', 'FILE2'], ['pre_FILE1', 'pre_FILE2'])
-])
-def test_rename_prepend(pre_args, pre_before, pre_after, tmpdir, monkeypatch):
-    monkeypatch.setattr('builtins.input', lambda x: 'y')
-    before_lst, after_lst = [], []
-    for before, after in pairwise(pre_before, pre_after, tmpdir):
-        before.write(before)
-        before_lst.append(before)
-        after_lst.append(after)
-
-        assert before.check(file=1)
-        assert after.check(file=0)
-
-    tmpdir.chdir()
-    args = parser.parse_args(pre_args)
-    main.main(args, parser)
-    for before, after in pairwise(before_lst, after_lst):
-        assert before.check(file=0)
-        assert after.check(file=1)
-        assert after.read() == before
-
-
-@pytest.mark.parametrize("sp_arg, sp_origpath, sp_dirpath, sp_fname, sp_res", [
-    # tests for space
-    # arg, origpath, dirpath, filename, expected result
-    ('_', '', '', 'file a', 'file_a'),
-    ('', '', '', 'file a', 'filea'),
-    ('.', '', '', 'file b', 'file.b'),
-    ('THIS', '', '', 'file c', 'fileTHISc'),
-    ('.', '', '', 'f i l e', 'f.i.l.e')
-])
-def test_filter_spaces(sp_arg, sp_origpath, sp_dirpath, sp_fname, sp_res):
-    args = parser.parse_args(['-sp', sp_arg])
-    filters = renamer.initfilters(args)
-    newname = renamer.runfilters(filters, sp_dirpath, sp_dirpath, sp_fname)
-    print('oldname: {} --> newname: {}'.format(sp_fname, newname))
-    assert newname == sp_res
-
-
-def test_filter_spaces_extra():
-    # special test case for spaces with no arguments
-    # sets spaces to _
-    args = parser.parse_args(['-sp'])
-    filters = renamer.initfilters(args)
-    newname = renamer.runfilters(filters, 'file a', '', 'file a')
-    print('oldname: {} --> newname: {}'.format('file a', newname))
-    assert newname == 'file_a'
-
-
-@pytest.mark.parametrize("c_arg, c_origpath, c_dirpath, c_fname, c_res", [
-    # tests for case
-    # case is only applied to the filename, not any extension
-    # which is why extensions aren't tested here
-    # arg, origpath, dirpath, filename, expected result
-    ('upper', '', '', 'file', 'FILE'),
-    ('upper', '', '', 'file1', 'FILE1'),
-    ('lower', '', '', 'FILE', 'file'),
-    ('lower', '', '', 'FILE1', 'file1'),
-    ('swap', '', '', 'FILE', 'file'),
-    ('swap', '', '', 'file', 'FILE'),
-    ('swap', '', '', 'fiLE', 'FIle'),
-    ('swap', '', '', 'FIle', 'fiLE'),
-    ('cap', '', '', 'FileName', 'Filename'),
-    ('cap', '', '', 'file name', 'File Name'),
-    ('cap', '', '', 'file1 1name', 'File1 1Name'),
-    ('cap', '', '', 'file1_1name', 'File1_1Name'),
-    ('cap', '', '', 'file1.1name', 'File1.1Name'),
-    ('upper  ', '', '', 'file', 'FILE'),
-    ('lower  ', '', '', 'FILE', 'file'),
-    ('swap   ', '', '', 'fIle', 'FiLE')
-])
-def test_filter_cases(c_arg, c_origpath, c_dirpath, c_fname, c_res):
-    args = parser.parse_args(['-c', c_arg])
-    filters = renamer.initfilters(args)
-    newname = renamer.runfilters(filters, c_dirpath, c_dirpath, c_fname)
-    print('oldname: {} --> newname: {}'.format(c_fname, newname))
-    assert newname == c_res
-
-
-@pytest.mark.parametrize("c_errarg", [
-    # errors for case
-    (['-v']),
-    (['bla', '-v']),
-    (['garbage', '-v']),
-    (['bug', '-v']),
-    (['1', '-v']),
-])
-def test_filter_case_err(c_errarg):
-    with pytest.raises(SystemExit) as err:
-        args = parser.parse_args(['-c', *c_errarg])
-        print(c_errarg, "is erroneous")
-        assert err.type == SystemExit
-
-
-@pytest.mark.parametrize("tr_arg, tr_origpath, tr_dirpath, tr_fname, tr_res", [
-    # tests for translate
-    # arg, origpath, dirpath, filename, expected result
-    (['a', 'b'], '', '', 'filea', 'fileb'),
-    (['fle', 'tbi'], '', '', 'filea', 'tibia'),
-    (['1', '2'], '', '', 'file1', 'file2')
-])
-def test_filter_translate(tr_arg, tr_origpath, tr_dirpath, tr_fname, tr_res):
-    args = parser.parse_args(['-tr', *tr_arg])
-    filters = renamer.initfilters(args)
-    newname = renamer.runfilters(filters, tr_origpath, tr_dirpath, tr_fname)
-    print('oldname: {} --> newname: {}'.format(tr_fname, newname))
-    assert newname == tr_res
-
-
-@pytest.mark.parametrize("tr_errargs", [
-    # errors for translate parser
-    # error if no values, both values are empty or not same length
-    # note: argparse handles len(args) > 2
-    (['-v']),
-    (['', '', '-v']),
-    (['a', '-v']),
-    (['a', 'bc', '-v']),
-    (['', 'bc', '-v'])
-])
-def test_filter_translate_err(tr_errargs):
-    with pytest.raises(SystemExit) as err:
-        args = parser.parse_args(['-tr', *tr_errargs])
-        print(tr_errargs, "is erroneous")
-        assert err.type == SystemExit
-
-
-@pytest.mark.parametrize("sl_arg, sl_origpath, sl_dirpath, sl_fname, sl_res", [
-    # tests for slice
-    # arg, origpath, dirpath, filename, expected result
-    (':4', '', '', 'filea', 'file'),
-    ('1:5', '', '', 'filea', 'ilea'),
-    ('2:', '', '', 'filea', 'lea')
-])
-def test_filter_slice(sl_arg, sl_origpath, sl_dirpath, sl_fname, sl_res):
-    args = parser.parse_args(['-sl', sl_arg])
-    filters = renamer.initfilters(args)
-    newname = renamer.runfilters(filters, sl_origpath, sl_dirpath, sl_fname)
-    print('oldname: {} --> newname: {}'.format(sl_fname, newname))
-    assert newname == sl_res
-
-
-@pytest.mark.parametrize("sl_errargs", [
-    # errors for slice parser
-    # error if slice is empty, cannot convert into slice object,
-    # value is not an integer
-    # note: argparse handles len(args) > 1
-    (['-v']),
-    (['', '-v']),
-    (['1:2:3:', '-v']),
-    (['1:2:3:4', '-v']),
-    (['1:2:3:4:', '-v']),
-    (['a:', '-v']),
-    (['a:1', '-v']),
-    (['1:b', '-v']),
-    ([':b', '-v']),
-    (['a:b', '-v']),
-    (['a:b:c', '-v']),
-    (['a::c', '-v']),
-    (['a:b:', '-v']),
-    (['1e:2b:3c', '-v'])
-])
-def test_filter_slice_err(sl_errargs):
-    with pytest.raises(SystemExit) as err:
-        args = parser.parse_args(['-sl', *sl_errargs])
-        print(sl_errargs, "is erroneous")
-        assert err.type == SystemExit
-
-
-@pytest.mark.parametrize("sh_arg, sh_origpath, sh_dirpath, sh_fname, sh_res", [
-    # tests for shave
-    # arg, origpath, dirpath, filename, expected result
-    (':1', '', '', 'filea', 'file'),
-    ('1:', '', '', 'filea', 'ilea'),
-    ('1:1', '', '', 'filea', 'ile')
-])
-def test_filter_shave(sh_arg, sh_origpath, sh_dirpath, sh_fname, sh_res):
-    args = parser.parse_args(['-sh', sh_arg])
-    filters = renamer.initfilters(args)
-    newname = renamer.runfilters(filters, sh_origpath, sh_dirpath, sh_fname)
-    print('oldname: {} --> newname: {}'.format(sh_fname, newname))
-    assert newname == sh_res
-
-
-@pytest.mark.parametrize("sh_errargs", [
-    # errors for shave parser
-    # error if value is empty, non-numeric character,
-    # more than two values, both values are none,
-    # any value is negative
-    # note: argparse handles len(args) > 1
-    (['-v']),
-    (['', '-v']),
-    (['a', '-v']),
-    (['a:', '-v']),
-    ([':b', '-v']),
-    (['a:b', '-v']),
-    (['1:b', '-v']),
-    (['a:1', '-v']),
-    (['1:2:3', '-v']),
-    (['1:2:3:', '-v']),
-    (['1:2:3:4', '-v']),
-    (['a:b:c', '-v']),
-    ([':', '-v']),
-    (['-1:', '-v']),
-    ([':-1', '-v']),
-    (['1:-1', '-v']),
-    (['-1:1', '-v']),
-    (['1:-1:5', '-v']),
-    (['-1:1:5', '-v']),
-])
-def test_filter_shave_err(sh_errargs):
-    with pytest.raises(SystemExit) as err:
-        args = parser.parse_args(['-sh', *sh_errargs])
-        print(sh_errargs, "is erroneous")
-        assert err.type == SystemExit
-
-
-@pytest.mark.parametrize("bracr_arg, bracr_origpath, bracr_dirpath, bracr_fname, bracr_res", [
-    # tests for bracket removal
-    # arg, origpath, dirpath, filename, expected result
-    (['round'], '', '', '(filea)file(file)', 'file'),
-    (['round'], '', '', 'filea', 'filea'),
-    (['round '], '', '', 'filea', 'filea'),
-    (['round'], '', '', '(file)(file)(file)file', 'file'),
-    (['round', '0'], '', '', '(file)(file)(file)file', 'file'),
-    (['round', '1'], '', '', '(file)file(file)', 'file(file)'),
-    (['  round  ', ' 1 '], '', '', '(file)file(file)', 'file(file)'),
-    (['round', '2'], '', '', '(file)file(file)', '(file)file'),
-    (['round', '3'], '', '', '(file)file(file)', '(file)file(file)'),
-    (['square', '1'], '', '', '[file]file[file]', 'file[file]'),
-    (['square', '2'], '', '', '[file]file[file]', '[file]file'),
-    (['square', '3'], '', '', '[file]file[file]', '[file]file[file]'),
-    (['curly', '1'], '', '', '{file}file{file}', 'file{file}'),
-    (['curly', '2'], '', '', '{file}file{file}', '{file}file'),
-    (['curly', '3'], '', '', '{file}file{file}', '{file}file{file}'),
-])
-def test_filter_bracr(bracr_arg, bracr_origpath, bracr_dirpath, bracr_fname, bracr_res):
-    args = parser.parse_args(['-bracr', *bracr_arg])
-    filters = renamer.initfilters(args)
-    newname = renamer.runfilters(filters, bracr_origpath, bracr_dirpath, bracr_fname)
-    print('oldname: {} --> newname: {}'.format(bracr_fname, newname))
-    print('expected:', bracr_res)
-    assert newname == bracr_res
-
-
 @pytest.mark.parametrize("bracr_rec_arg, bracr_type", [
-    # special test cases for bracr
-    # bracr doesn't handle recursive brackets and should fail
     ('((file))file', 'round'),
     ('[[file]]file', 'square'),
     ('{{file}}file', 'curly')
@@ -373,31 +213,6 @@ def test_filter_bracr_extra(bracr_rec_arg, bracr_type):
     filters = renamer.initfilters(args)
     newname = renamer.runfilters(filters, '', '', bracr_rec_arg)
     assert newname != 'file'
-
-
-@pytest.mark.parametrize("bracr_errargs", [
-    # errors for bracket remove
-    # error if no arguments, too many arguments
-    # first argument not in (round, square, curly)
-    # second argument is not an integer >= 0
-    # nargs = *, so argparse doesn't handle argument lengths
-    (['-v']),
-    (['something', '-v']),
-    (['roun', '-v']),
-    (['CURLY', '-v']),
-    (['ROUND', '-v']),
-    (['SQUARE', '-v']),
-    (['curly', 'HERE', '-v']),
-    (['curly', '', '-v']),
-    (['curly', '1a', '-v']),
-    (['curly', '-1', '-v']),
-    (['curly', '1', '2', '-v'])     # len(args) > 2
-])
-def test_filter_bracr_err(bracr_errargs):
-    with pytest.raises(SystemExit) as err:
-        args = parser.parse_args(['-bracr', *bracr_errargs])
-        print(bracr_errargs, "is erroneous")
-        assert err.type == SystemExit
 
 
 @pytest.mark.parametrize("re_arg, re_origpath, re_dirpath, re_fname, re_res", [
@@ -422,18 +237,4 @@ def test_filter_regex(re_arg, re_origpath, re_dirpath, re_fname, re_res):
     print('oldname: {} --> newname: {}'.format(re_fname, newname))
     print('expected:', re_res)
     assert newname == re_res
-
-
-@pytest.mark.parametrize("re_errargs", [
-    # errors for regex
-    # nargs = *, so argparse doesn't handle argument lengths
-    (['-v']),
-    (['', '-v']),
-    (['a', 'b', '2', '2', '-v'])  # len(args) > 3
-])
-def test_filter_re_err(re_errargs):
-    with pytest.raises(SystemExit) as err:
-        args = parser.parse_args(['-re', *re_errargs])
-        print(re_errargs, "is erroneous")
-        assert err.type == SystemExit
 """
